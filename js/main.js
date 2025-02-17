ad3e4a6da9468761bbef72957818380e592b8f80
yana <taratynova.yana@bk.ru>
2025-02-18 00:24:17 +0700

    3 practic

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ let app = new Vue({
         description: "A pair of warm, fuzzy socks",
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks."
+        inStock: true,
+        inventory: 100,
+        onSale: true
     }
 })
