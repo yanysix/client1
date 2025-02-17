bcf2952ebed9f4944abf717138ce9577a4001b0a
yana <taratynova.yana@bk.ru>
2025-02-18 00:31:09 +0700

    4 practic

--- js/main.js
+++ js/main.js
@@ -6,7 +6,17 @@ let app = new Vue({
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
         inStock: true,
-        inventory: 100,
-        onSale: true
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
 })
