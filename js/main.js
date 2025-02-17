9619dcea2fc5c5326cddc3fb1265b4b0758ed93f
yana <taratynova.yana@bk.ru>
2025-02-18 00:03:07 +0700

    2 practic

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        description: "A pair of warm, fuzzy socks"
+        description: "A pair of warm, fuzzy socks",
+        image: "./assets/vmSocks-blue-onWhite.jpg",
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks."
     }
 })
